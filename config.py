b299ff38d446073f7ff875c0f0fd77d5d327b356
Jeffrey Moore <jeffmooreweb@gmail.com>
2023-05-27 19:34:40 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,220 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+
+import subprocess
+
+mod = "mod1"
+terminal = "kitty"
+
+keys = [
+    # focus
+    Key([mod], "j", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "k", lazy.layout.right(), desc="Move focus to right"),
+
+    # move
+    Key([mod, "shift"],
+        "h",
+        lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+
+    # grow
+    Key([mod],
+        "h",
+        lazy.layout.decrease_ratio(),
+        desc="Grow window to the left"),
+    Key([mod],
+        "l",
+        lazy.layout.increase_ratio(),
+        desc="Grow window to the right"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+    # spawn
+    Key([mod, "shift"],
+        "Return",
+        lazy.spawn(terminal),
+        desc="Launch terminal (Bash)"),
+    Key([mod],
+        "Return",
+        lazy.spawn(f"{terminal} -e fish"),
+        desc="Launch terminal (Fish)"),
+    Key([mod],
+        "p",
+        lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
+
+    # process
+    Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+
+    # layout
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+]
+
+groups = [Group(i) for i in "123456789"]
+
+for i in groups:
+    keys.extend([
+        Key(
+            [mod],
+            i.name,
+            lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name),
+        ),
+        Key(
+            [mod, "shift"],
+            i.name,
+            lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name),
+        ),
+    ])
+
+layouts = [
+    layout.Tile(border_focus="#81A1C1",
+                border_on_single=True,
+                border_width=2,
+                margin=2,
+                margin_on_single=True),
+    layout.Max(
+        border_focus="#81A1C1",
+        border_width=2,
+        margin=2,
+    ),
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font="Noto Sans Mono",
+    fontsize=14,
+    padding=3,
+    margin_x=8,
+)
+extension_defaults = widget_defaults.copy()
+screens = [
+    Screen(top=bar.Bar(
+        [
+            widget.CurrentLayoutIcon(),
+            widget.GroupBox(this_current_screen_border="#81A1C1"),
+            widget.Sep(foreground="#81A1C1", size_percent=60),
+            widget.Prompt(),
+            widget.WindowName(format=" {name} "),
+            widget.Chord(
+                chords_colors={
+                    "launch": ("#ff0000", "#ffffff"),
+                },
+                name_transform=lambda name: name.upper(),
+            ),
+            widget.Sep(foreground="#81A1C1", size_percent=60),
+            widget.TextBox(" CPU"),
+            widget.CPUGraph(graph_color="#81A1C1", fill_color="#81A1C1"),
+            widget.Sep(foreground="#81A1C1", size_percent=60),
+            widget.TextBox(" Memory"),
+            widget.MemoryGraph(graph_color="#81A1C1", fill_color="#81A1C1"),
+            widget.Sep(foreground="#81A1C1", size_percent=60),
+            widget.PulseVolume(emoji=True, fmt=" {}"),
+            widget.GenPollText(update_interval=1,
+                               func=lambda:
+                               f"{subprocess.check_output('/home/jeff/python/volume.py').decode().strip():>4} "),
+            widget.Sep(foreground="#81A1C1", size_percent=60),
+            widget.Battery(format=" 🔋 {percent:>4.0%} ", show_short_text=False),
+            widget.Sep(foreground="#81A1C1", size_percent=60),
+            widget.Wlan(interface="wlp2s0", format=" 🛜  {essid} "),
+            widget.Sep(foreground="#81A1C1", size_percent=60),
+            widget.OpenWeather(location="Dallas", format=" {icon}"),
+            widget.Clock(format=" %m/%d/%y %a %I:%M %p "),
+        ],
+        32,
+        fontsize=16,
+        foreground="#EEEEEE",
+        background="#2E3440",
+        margin=[0, 0, 1, 0],
+    ), ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod],
+         "Button1",
+         lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod],
+         "Button3",
+         lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    Match(wm_class="confirmreset"),  # gitk
+    Match(wm_class="makebranch"),  # gitk
+    Match(wm_class="maketag"),  # gitk
+    Match(wm_class="ssh-askpass"),  # ssh-askpass
+    Match(title="branchdialog"),  # gitk
+    Match(title="pinentry"),  # GPG key password entry
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
